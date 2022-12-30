b4d370654f0934afded704b3f16a3a74f1c628f1
Vikas Gupta <digitalvikas22@gmail.com>
2022-12-30 02:14:34 +0000

    Practice

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,24 @@
+import './index.css'
+
+const DenominationItem = props => {
+  const {denominationDetails, updateBalance} = props
+  const {value} = denominationDetails
+
+  const onClickDenomination = () => {
+    updateBalance(value)
+  }
+
+  return (
+    <li className="number-container">
+      <button
+        className="button-number"
+        type="button"
+        onClick={onClickDenomination}
+      >
+        {value}
+      </button>
+    </li>
+  )
+}
+
+export default DenominationItem
